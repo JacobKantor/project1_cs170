4698ef5ec3a6549a71f114728ba06312a474bdb7
JacobKantor <jacobjkantor@gmail.com>
2026-02-12 14:45:07 -0800

    same commit containing additional comments and output etc., forgot to save first

--- nPuzzle.cpp
+++ nPuzzle.cpp
@@ -8,61 +8,54 @@
 using namespace std;
 using namespace std::chrono;
 
-// Hard coded preloaded puzzles the user can select to compute
+// Hard coded puzzles the user can select to compute
 vector<vector<int>> trivial1 = {
     {1, 2, 3},
     {4, 5, 6},
     {7, 8, 0}
 };
-
 vector<vector<int>> trivial2 = {
     {1, 2, 3},
     {4, 5, 6},
     {0, 7, 8} 
 };
-
 vector<vector<int>> easy1 = {
     {1, 2, 3},
     {5, 0, 6},
     {4, 7, 8}
 };
-
 vector<vector<int>> easy2 = {
     {1, 3, 6},
     {5, 0, 2},
     {4, 7, 8}
 };
-
 vector<vector<int>> med1 = {
     {1, 3, 6},
     {5, 0, 7},
     {4, 8, 2}
 };
-
 vector<vector<int>> med2 = {
     {1, 6, 7},
     {5, 0, 3},
     {4, 8, 2}
 };
-
 vector<vector<int>> hard1 = {
     {7, 1, 2},
     {4, 8, 5},
     {6, 3, 0}
 };
-
 vector<vector<int>> hard2 = {
     {0, 7, 2},
     {4, 6, 1},
     {3, 5, 8}
 };
-
 vector<vector<int>> goalState = {
     {1, 2, 3},
     {4, 5, 6},
     {7, 8, 0}
 };
 
+// Object for handling the variables puzzle board, parent, depth, and cost for each respective state
 struct TreeNode{
     vector<vector<int>> board;
     TreeNode *parent;    // pointer to a parent of the matrices type
@@ -74,7 +67,7 @@ struct TreeNode{
         :board(node), parent(parent), depth(depth), cost(cost){}
 };
 
-// Comparisons for priority queue min heap to know the next best node
+// Object so that the priority queue knows what variable (cost) to order on
 struct Compare{
     bool operator()(const TreeNode* a, const TreeNode* b) const{
         return a->cost > b->cost;
@@ -100,8 +93,7 @@ int main() {
     
     if (puzzleType == 1){
         selectSearchAlgorithm(selectPuzzle());  // user chooses an 8-puzzle from the hard coded vectors, and an algorithm
-    }else if (puzzleType == 2){
-        // Create unique puzzle
+    }else if (puzzleType == 2){                 // Create unique puzzle
         vector<int> row1, row2, row3;
         cout << "Please enter your custom 8-puzzle over valid digits 0-8, where 0 indicates the blank tile." << endl;
         cout << "Enter the three digits for row 1 (enter after each digit)" << endl;
@@ -136,7 +128,7 @@ int main() {
     return 0;
 }
 
-// User enters a number that indicates the difficulty premade 8-puzzle selected
+// User enters a number that indicates the difficulty which correlates to the premade hard coded 8-puzzles
 vector<vector<int>> selectPuzzle(){
     int userPuzzle = 0;
     
@@ -219,28 +211,28 @@ void selectSearchAlgorithm(const vector<vector<int>>& puzzle){
 }
 
 void uniformCostSearch(const vector<vector<int>>& puzzle, int heuristic){
-    TreeNode* startState = new TreeNode(puzzle, nullptr, 0, 0);    
-    priority_queue<TreeNode*, vector<TreeNode*>, Compare> priorityQueue;                          // Creates a First In First Out data structure for BFS
+    TreeNode* startState = new TreeNode(puzzle, nullptr, 0, 0);                                   // first state in the working queue
+    priority_queue<TreeNode*, vector<TreeNode*>, Compare> priorityQueue;                          // The priority queue will act as FIFO with uniform cost
     priorityQueue.push(startState);                          // Start the working queue with the initial puzzle
     
-    set<vector<vector<int>>> visited;               // Keeps track of children to prevent repeat visits to the identical states
+    set<vector<vector<int>>> visited;               // Keeps track of states fully explored to prevent repeat visits to the identical states
     int currQueueSize = 1;                          // 1 so far since the root startState
-    int maxQueueSize = 1;                           // compare with current for updating
-    int maxNodesExpanded = 0;                       // updates each time all 4 possible children are checked for the current board
+    int maxQueueSize = 1;                           // compares with current max queue size for updating
+    int maxNodesExpanded = 0;                       // updates each time all possible children of a state are explored for the current board
     
     // declare clock start and stop variables
     high_resolution_clock::time_point start;
     high_resolution_clock::time_point stop;
-    start = high_resolution_clock::now();                    // starts the timer
+    start = high_resolution_clock::now();                  // starts the timer
 
-    while(!priorityQueue.empty()){                           // If the working queue is empty, then I have checked the entire search space and found no answer (or the puzzle was invalid)
+    while(!priorityQueue.empty()){                         // If the working queue is empty, then I have checked the entire search space and found no answer (or the puzzle was invalid)
         TreeNode* currNode = priorityQueue.top();          // Store temporarily to perform operations then move to visited, also used to store parent for children
-        priorityQueue.pop();                                 // Remove the current node from priorityQueue
+        priorityQueue.pop();                               // Remove the current node from priorityQueue
         maxQueueSize--;
 
         if (!visited.count(currNode->board)){           // if this is a new board, expand, otherwise skip to next board state iteration
             visited.insert(currNode->board);            // Now that the currBoard has been popped and is being expanded, add it to visited
-            int hOfN = 0;
+            int hOfN = 0;                               // Set to corresponding heuristic, and output the h(n) details for the current state
             if (heuristic == 1){
                 hOfN = misplacedTile(currNode->board);
             }else if (heuristic == 2){
@@ -249,16 +241,17 @@ void uniformCostSearch(const vector<vector<int>>& puzzle, int heuristic){
             cout << "The best state to expand with a g(n) = " << currNode->depth << " and h(n) = " << hOfN << " is:" << endl;
             printPuzzle(currNode->board);
 
-            if (currNode->board == goalState){          // If goalState is found, return the BFS 
-                stop = high_resolution_clock::now();             // ends timer
+            if (currNode->board == goalState){                      // If goalState is found...
+                stop = high_resolution_clock::now();                // ends timer
                 duration<double, milli> duration = stop - start;    // converts the difference of time to double milliseconds type
+                
+                // output computation details
                 cout << "Puzzle Solved!" << endl << endl;
-                // printPuzzle(currNode->board);           // change to show path later?
                 cout << "Solution depth was : " << currNode->depth << endl;
                 cout << "Number of nodes expanded: " << maxNodesExpanded << endl;
                 cout << "Max queue size: " << maxQueueSize << endl;
                 cout << "Completion Time: " << duration.count() << " milliseconds" << endl;
-                return;                                 // end search now that the goal state is reached
+                return;                                             // end search now that the goal state is reached
             }
 
 
@@ -274,12 +267,11 @@ void uniformCostSearch(const vector<vector<int>>& puzzle, int heuristic){
                 }
             }
 
-            // Check for valid swaps by using a 3x3 bound for the matrices
-            // Check up swap
-            // ADD FUNCTION FOR HEURISTICS TO COSTs
+            // 4 Checks for valid swaps (up/down/left/right) by using a 3x3 bound for the matrices
+            // Check if up swap is valid, and create the child state with the proper cost according the heuristic
             if (zeroI - 1 >= 0){    // if this condition is valid, then I need to make a new child with a board and link to parent
-                vector<vector<int>> childBoard = currNode->board;
-                swap(childBoard[zeroI][zeroJ], childBoard[zeroI - 1][zeroJ]);
+                vector<vector<int>> childBoard = currNode->board;                           // create the new board premove
+                swap(childBoard[zeroI][zeroJ], childBoard[zeroI - 1][zeroJ]);               // slide the upper tile into 0 position
                 if (heuristic == 0){                                          // put the new child on the working queue, updating cost according to the heuristic selected
                     priorityQueue.push(new TreeNode(childBoard, currNode, currNode->depth + 1, currNode->cost + 1));  
                 }else if (heuristic == 1){
@@ -289,7 +281,7 @@ void uniformCostSearch(const vector<vector<int>>& puzzle, int heuristic){
                 }
                 currQueueSize++;    // keep track of pushes to the queue
             }
-            // Check down swap
+            // Check if down swap is valid, and create the child state with the proper cost according the heuristic
             if (zeroI + 1 <= 2){ 
                 vector<vector<int>> childBoard = currNode->board;
                 swap(childBoard[zeroI][zeroJ], childBoard[zeroI + 1][zeroJ]);
@@ -302,7 +294,7 @@ void uniformCostSearch(const vector<vector<int>>& puzzle, int heuristic){
                 }
                 currQueueSize++;
             }
-            // Check left swap
+            // Check if left swap is valid, and create the child state with the proper cost according the heuristic
             if (zeroJ - 1 >= 0){
                 vector<vector<int>> childBoard = currNode->board;
                 swap(childBoard[zeroI][zeroJ], childBoard[zeroI][zeroJ - 1]);
@@ -315,11 +307,11 @@ void uniformCostSearch(const vector<vector<int>>& puzzle, int heuristic){
                 }
                 currQueueSize++;
             }
-            // Check right swap
+            // Check if right swap is valid, and create the child state with the proper cost according the heuristic
             if (zeroJ + 1 <= 2){
                 vector<vector<int>> childBoard = currNode->board;
                 swap(childBoard[zeroI][zeroJ], childBoard[zeroI][zeroJ + 1]);
-                if (heuristic == 0){                                          // put the new child on the working queue, updating cost according to the heuristic selected
+                if (heuristic == 0){                                         // put the new child on the working queue, updating cost according to the heuristic selected
                     priorityQueue.push(new TreeNode(childBoard, currNode, currNode->depth + 1, currNode->cost + 1));  
                 }else if (heuristic == 1){
                     priorityQueue.push(new TreeNode(childBoard, currNode, currNode->depth + 1, currNode->depth + 1 + misplacedTile(childBoard)));  
@@ -337,6 +329,7 @@ void uniformCostSearch(const vector<vector<int>>& puzzle, int heuristic){
     cout << "failure, there is no solution in the search space, or the 8-puzzle is invalid" << endl;
 };
 
+// Counts the number of that aren't in its solved state
 int misplacedTile(const vector<vector<int>>& childBoard){
     int misplaced = 0;
     for (int i = 0; i < 3; i++){                            // traverse each position of a board
@@ -346,10 +339,10 @@ int misplacedTile(const vector<vector<int>>& childBoard){
             }
         }
     }
-    
     return misplaced;
 }
 
+// Totals the counts of distance from tiles to their solved state
 int manhattan(const vector<vector<int>>& childBoard){
     int distance = 0;               // cost is the difference in distance from the goal position
     int goalNum = 0;                // stores the value at the indices for the board goalState
@@ -373,6 +366,5 @@ int manhattan(const vector<vector<int>>& childBoard){
             }
         }
     }
-
     return distance;
 }
